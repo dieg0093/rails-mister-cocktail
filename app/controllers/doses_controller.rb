51cdacdfde5626472593c8d829bacbcead6f31cb
/code/$GITHUB_USERNAME/dotfiles <diego.delaminne@gmail.com>
2018-11-01 18:35:14 +0100

    cocktails

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,24 @@
+class DosesController < ApplicationController
+
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+    @ingredients = Ingredient.all
+
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    @dose.save
+    redirect_to cocktails_path
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id, :cocktail_id)
+  end
+
+end
